2b3f097e839e757841611e01a7d7fa82227ed7a7
Kevin <testcn@vip.qq.com>
2013-09-11 20:52:08 +0800

    2013-09-08 v0.8 What's new: ============================== 1.新增log分析功能，可以自动统计Crash数量。 2.更新adb检测方式，使用adb get-serialno检查设备是否存在，并返回设备号。 3.更新测试结果输出保存路径，方便区分每次测试结果，注意Report不在此路径下。 4.将log分析和adb检查两个功能封装成模块，方便调试和调用。

---
+++ adbCmd.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*
+__author__ = 'Kevin'
+
+from subprocess import Popen, PIPE
+
+def adbSerialno(s):
+    resp = Popen(s, shell=True,stdout=PIPE, stderr=PIPE).stdout.readlines()
+    a = []
+    for i in resp:
+        a.append(i.strip('\r\n'))
+    return a
+
+if __name__ == '__main__':
+    r = adbSerialno('adb get-serialno')
+    if r[0] == 'unknown':
+        print 'No devices is connected.'
+    elif len(r) == 1:
+        print r[0]
+    else:
+        print 'Multiple devices connected.'
