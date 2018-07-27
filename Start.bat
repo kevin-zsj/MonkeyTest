145d8e5638fdff108e2ee2c2f0a72ac596a6f292
Kevin <testcn@vip.qq.com>
2018-07-27 14:21:00 +0800

    更新Monkey运行方式

---
+++ Start.bat
@@ -0,0 +1,14 @@
+@echo off
+set local_path=%cd%
+
+adb remount
+echo Pushing Test tools ...
+adb push %local_path%\startMonkey.sh /sdcard/
+echo Pushing analysis tools ...
+adb push %local_path%\event_counter.sh /sdcard/
+echo Pushing black names ...
+adb push %local_path%\data\black.txt /sdcard/
+echo Run test ...
+adb shell "sh /sdcard/startMonkey.sh &"
+
+pause
---
+++ startMonkey.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+i=25;
+while [[ $i -gt 0 ]];do
+	echo "time " $i "start...";
+	dt=`date +%H%M%S`;
+
+	pid_logcat=`ps -A|grep logcat|grep -v "grep"|busybox awk '{print $2}'`
+	for id3 in $pid_logcat 
+	do 
+   		kill -9 $id3 
+   		echo "kill logcat" 
+	done
+	logcat >> /mnt/sdcard/logcat_$dt.txt &
+
+	monkey --ignore-timeouts --ignore-security-exceptions --pkg-blacklist-file /sdcard/black.txt --throttle 500 -s $dt -v -v -v 125000 >> /sdcard/monkey_log_$dt.txt;
+	((i--));
+	sleep 10;
+	echo "time " $i "finished";
+done;
